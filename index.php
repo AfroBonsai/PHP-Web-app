d9e591c8223bad74fdbdd46fa7cc60a37ac238fb
Alfonso Lara <86298325+Yuiitwitch@users.noreply.github.com>
2021-11-25 12:40:07 +0100

    Creamos los botones del index para poder navegar entre vistas

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+echo '<a href="http://localhost/dev/PHP-WEB-APP/pages/cartaPlatos.php">
+  <style>button{cursor: pointer}</style>
+  <button type="button">CartaPlatos</button>
+  </a>';
+  echo '<a href="http://localhost/dev/PHP-WEB-APP/pages/login.php">
+  <style>button{cursor: pointer}</style>
+  <button type="button">Login</button>
+  </a>';
+  echo '<a href="http://localhost/dev/PHP-WEB-APP/pages/perfilUsuario.php">
+  <style>button{cursor: pointer}</style>
+  <button type="button">Perfil</button>
+  </a>';
+  echo '<a href="http://localhost/dev/PHP-WEB-APP/pages/perfilUsuario.php">
+  <style>button{cursor: pointer}</style>
+  <button type="button">Perfil</button>
+  </a>';
+  ?>
+
+
+<?php
+
+?>
+
+
+<?php
+
+?>
