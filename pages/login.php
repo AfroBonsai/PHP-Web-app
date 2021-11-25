8ad69ad8fd2cf9e88cc73d4e393c90236da68b5a
Arnoleitor <arnold201991@gmail.com>
2021-11-25 12:17:56 +0100

    Login implementado

---
+++ pages/login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="plato.php" method="post">
+        <p>Email: <input type="text" name="Email" /></p>
+        <p>Contraseña: <input type="password" name="Contraseña" /></p>
+        <style>.button{cursor: pointer}</style>
+        <p><input type="submit" class="button"/></p>
+    </form>
+</body>
+</html>
