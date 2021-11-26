2ab5fc86ed0bc393a92ddd85d3b8a9edaf2d8b2d
Arnoleitor <arnold201991@gmail.com>
2021-11-26 10:07:48 +0100

    diseño login

--- pages/loginUsuario.php
+++ pages/loginUsuario.php
@@ -4,14 +4,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../styles/index.css">
     <title>Login</title>
 </head>
 <body>
-    <form action="plato.php" method="post">
+    <div class="Login"> 
+    <form action="loginUsuario.php" method="post">
         <p>Email: <input type="text" name="Email" /></p>
         <p>Contraseña: <input type="password" name="Contraseña" /></p>
         <style>.button{cursor: pointer}</style>
         <p><input type="submit" value="Login" class="button"/></p>
+        </div>
     </form>
 </body>
 </html>
