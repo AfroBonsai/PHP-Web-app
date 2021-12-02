b868a011450369f9f0fdf9082ceedda48f358dff
AfroBonsai <geozam89@gmail.com>
2021-12-02 20:31:30 +0100

    cambio conexion

--- database/db_connect.php
+++ database/db_connect.php
@@ -2,6 +2,17 @@
 <!-- mysql://b1c66af0b95655:0182c2e6@eu-cdbr-west-01.cleardb.com/heroku_aa142db5d8feb81?reconnect=true -->
 
 <?php
+function conectar(){
+//Conexion con base de datos
+$mysqli = new mysqli("eu-cdbr-west-01.cleardb.com:3306", "b1c66af0b95655","0182c2e6", "heroku_aa142db5d8feb81");
+if ($mysqli->connect_errno) {
+    echo "Falló la conexión con MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+return $mysqli;
+}
+?>
+
+<!--  
 $url = parse_url(getenv("mysql://b1c66af0b95655:0182c2e6@eu-cdbr-west-01.cleardb.com/heroku_aa142db5d8feb81?reconnect=true"));
 
 $server = $url["host"];
@@ -10,7 +21,7 @@ $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
-?>
+-->
 
 
 <!-- $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
