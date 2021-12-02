cc95633e36bae7f55226138bd87b13b5b5830610
AfroBonsai <geozam89@gmail.com>
2021-12-02 19:07:50 +0100

    corrección conexion base de datos

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,19 +1,4 @@
-
-<!-- mysql://b1c66af0b95655:0182c2e6@eu-cdbr-west-01.cleardb.com/heroku_aa142db5d8feb81?reconnect=true -->
-
-<?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-
-$server = $url["eu-cdbr-west-01.cleardb.com"];
-$username = $url["b1c66af0b95655"];
-$password = $url["0182c2e6"];
-$db = substr($url["heroku_aa142db5d8feb81"], 1);
-
-$conn = new mysqli($server, $username, $password, $db);
-?>
-
-
-<!-- 
+<!-- <?php
 function conectar(){
     //conexion con la base de datos
 
@@ -25,4 +10,19 @@ echo "Falló la conexión con MySQL: (" . $mysqli->connect_errno . ") "
 }
 return $mysqli;
 }
+
+?>
 -->
+
+<!-- mysql://b1c66af0b95655:0182c2e6@eu-cdbr-west-01.cleardb.com/heroku_aa142db5d8feb81?reconnect=true -->
+
+<?php
+$url = parse_url(getenv("mysql://b1c66af0b95655:0182c2e6@eu-cdbr-west-01.cleardb.com/heroku_aa142db5d8feb81?reconnect=true"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+?>
