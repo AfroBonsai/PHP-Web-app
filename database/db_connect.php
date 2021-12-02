dd4a8a209c62f625eeb5a56096703d830d70e530
AfroBonsai <geozam89@gmail.com>
2021-12-02 18:56:49 +0100

    corregido conexión base de datos y link logout

--- database/db_connect.php
+++ database/db_connect.php
@@ -1,14 +1,28 @@
+
+<!-- mysql://b1c66af0b95655:0182c2e6@eu-cdbr-west-01.cleardb.com/heroku_aa142db5d8feb81?reconnect=true -->
+
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["eu-cdbr-west-01.cleardb.com"];
+$username = $url["b1c66af0b95655"];
+$password = $url["0182c2e6"];
+$db = substr($url["heroku_aa142db5d8feb81"], 1);
+
+$conn = new mysqli($server, $username, $password, $db);
+?>
+
+
+<!-- 
 function conectar(){
     //conexion con la base de datos
 
 $mysqli = new mysqli ("eu-cdbr-west-01.cleardb.com:3306", "b1c66af0b95655", "0182c2e6", "heroku_aa142db5d8feb81"); 
-//Control de errores
+//Control de errores   eu-cdbr-west-01.cleardb.com         b1c66af0b95655    0182c2e6    heroku_aa142db5d8feb81
 if ($mysqli->connect_errno) {
 echo "Falló la conexión con MySQL: (" . $mysqli->connect_errno . ") "
 . $mysqli->connect_error;
 }
 return $mysqli;
 }
-
-?>
+-->
