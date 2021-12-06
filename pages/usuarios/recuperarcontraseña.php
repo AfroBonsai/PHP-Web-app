35fe09912a70e935d543d94ce9858c4db6bfe01a
Alfonso Lara <86298325+Yuiitwitch@users.noreply.github.com>
2021-12-06 13:48:30 +0100

    cambios en recueperar contraseña

--- pages/usuarios/recuperarcontraseña.php
+++ pages/usuarios/recuperarcontraseña.php
@@ -2,8 +2,8 @@
 include '../header.php';
 ?>
 
-<div class="main flex">
-  <form class="form flexColumn" action="recuperar.php" method="post">
+<div class="main flex d-flex justify-content-center text-center align-items-center">
+  <form class="form flexColumn " action="recuperar.php" method="post">
     <h1>Login</h1>
     <input type="text" name="email" placeholder="email">
     <input type="submit" value="Recuperar" class="boton" />
