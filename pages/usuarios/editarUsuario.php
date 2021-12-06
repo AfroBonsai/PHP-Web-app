58c97a7b83c94850c3b469d53f2d574c7599d6ca
Alfonso Lara <86298325+Yuiitwitch@users.noreply.github.com>
2021-12-06 22:10:25 +0100

    cambios en mostrarUsuarios para poder visualziar y añadir boostrap

--- pages/usuarios/editarUsuario.php
+++ pages/usuarios/editarUsuario.php
@@ -6,9 +6,9 @@
         }
 
     echo '
-        <div class="main flexColumn">
-            <form class="form flexColumn" action="editar.php" method="post">
-            <h1>Editar ' . ($_GET["nombre"]) . '</h1>
+        <div class="main d-flex justify-content-center text-center align-items-center bgmain">
+            <form class="shadow-lg p-3 mb-5 border  rounded" action="editar.php" method="post">
+            <h1  class="p-3">Editar ' . ($_GET["nombre"]) . '</h1>
         ';
     $id=($_GET["id"]);
     $nombre=($_GET["nombre"]);
@@ -17,18 +17,31 @@
 
 
 ?>
-
+        <div >
         <label for="nombre">Nombre</label>
+        </div>
+        <div class="p-3">
         <input type="text" name="nombre" value="<?php echo $nombre;?>" pattern="^[A-Za-z]{3,16}$" title="Min. 3 caracteres y solo admite letras" required/>
+        </div>
+        <div>
         <label for="email">Email</label>
+        </div>
+        <div class="p-3">
         <input type="email" name="email" value="<?php echo $email;?>" required/>
+        </div>
+        <div>
         <label for="accesoAdmin">Admin</label>
+        </div>
+        <div>
         <select name="accesoAdmin">
             <option value="0">Usuario</option>
             <option value="1">Admin</option>  
         </select>
+        </div>
+        <div class="p-5">
         <input type="hidden" name="id" value="<?php echo $id;?>" />
-        <input type="submit" value="Editar" class="boton" />
-        <a class="boton flex" href="./mostrarUsuarios.php">Atras</a>
+        <input type="submit" value="Editar" class="btn-primary btn-sm rounded-pill w-15" />
+        <a class="btn-info btn-sm rounded-pill w-15" href="./mostrarUsuarios.php">Atras</a>
+        </div>
     </form>
 </div>
