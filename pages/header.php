c3a781b6cc40de54ea00f22255a9f63480330a0b
AfroBonsai <geozam89@gmail.com>
2021-12-07 11:38:34 +0100

    adaptado links al deploy

--- pages/header.php
+++ pages/header.php
@@ -12,7 +12,7 @@
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-            <link rel="stylesheet" href="http://localhost:80/dev/PHP-Web-app/styles/index.css">
+            <link rel="stylesheet" href="https://aadg-api-restaurante.herokuapp.com/styles/index.css">
             <title>Php-Web-App</title>
           </head>
 
@@ -25,10 +25,10 @@
                 <div class="collapse navbar-collapse justify-content-md-center" id="collapse">
                   <ul class="navbar-nav">
                     <li class="nav-item">
-                      <a class="nav-link" aria-current="page" href="http://localhost:80/dev/PHP-Web-app/pages/usuarios/recuperarContraseña.php">Recuperar Contraseña</a>
+                      <a class="nav-link" aria-current="page" href="https://aadg-api-restaurante.herokuapp.com/pages/usuarios/recuperarContraseña.php">Recuperar Contraseña</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="http://localhost:80/dev/PHP-Web-app/pages/usuarios/registroUsuario.php">Registro</a>
+                      <a class="nav-link" href="https://aadg-api-restaurante.herokuapp.com/pages/usuarios/registroUsuario.php">Registro</a>
                     </li>
                   </ul>
                 </div>
@@ -48,7 +48,7 @@
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-            <link rel="stylesheet" href="http://localhost:80/dev/PHP-Web-app/styles/index.css">
+            <link rel="stylesheet" href="https://aadg-api-restaurante.herokuapp.com/styles/index.css">
             <title>Document</title>
           </head>
 
@@ -61,13 +61,13 @@
                 <div class="collapse navbar-collapse justify-content-md-center" id="collapse">
                   <ul class="navbar-nav">
                     <li class="nav-item">
-                      <a class="nav-link" aria-current="page" href="http://localhost:80/dev/PHP-Web-app/pages/user/cartaPlatos.php">Carta</a>
+                      <a class="nav-link" aria-current="page" href="https://aadg-api-restaurante.herokuapp.com/pages/user/cartaPlatos.php">Carta</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="http://localhost:80/dev/PHP-Web-app/pages/usuarios/perfilUsuario.php">Perfil</a>
+                      <a class="nav-link" href="https://aadg-api-restaurante.herokuapp.com/pages/usuarios/perfilUsuario.php">Perfil</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="http://localhost:80/dev/PHP-Web-app/pages/usuarios/logout.php">Logout</a>
+                      <a class="nav-link" href="https://aadg-api-restaurante.herokuapp.com/usuarios/logout.php">Logout</a>
                     </li>
                   </ul>
                 </div>
@@ -87,7 +87,7 @@
               <meta name="viewport" content="width=device-width, initial-scale=1.0">
               <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
               <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-              <link rel="stylesheet" href="http://localhost:80/dev/PHP-Web-app/styles/index.css">
+              <link rel="stylesheet" href="https://aadg-api-restaurante.herokuapp.com/styles/index.css">
               <title>Document</title>
             </head>
 
@@ -100,19 +100,19 @@
                 <div class="collapse navbar-collapse justify-content-md-center" id="collapse">
                   <ul class="navbar-nav">
                     <li class="nav-item">
-                      <a class="nav-link" aria-current="page" href="http://localhost:80/dev/PHP-Web-app/pages/platos/cartaPlatos.php">Carta</a>
+                      <a class="nav-link" aria-current="page" href="https://aadg-api-restaurante.herokuapp.com/pages/platos/cartaPlatos.php">Carta</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="http://localhost:80/dev/PHP-Web-app/pages/usuarios/mostrarUsuarios.php">Usuarios</a>
+                      <a class="nav-link" href="https://aadg-api-restaurante.herokuapp.com/pages/usuarios/mostrarUsuarios.php">Usuarios</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="http://localhost:80/dev/PHP-Web-app/pages/ingredientes/mostrarIngrediente.php">Ingredientes</a>
+                      <a class="nav-link" href="https://aadg-api-restaurante.herokuapp.com/pages/ingredientes/mostrarIngrediente.php">Ingredientes</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="http://localhost:80/dev/PHP-Web-app/pages/usuarios/perfilUsuario.php">Perfil</a>
+                      <a class="nav-link" href="https://aadg-api-restaurante.herokuapp.com/pages/usuarios/perfilUsuario.php">Perfil</a>
                     </li>
                     <li class="nav-item">
-                      <a class="nav-link" href="http://localhost:80/dev/PHP-Web-app/pages/usuarios/logout.php">Logout</a>
+                      <a class="nav-link" href="https://aadg-api-restaurante.herokuapp.com/pages/usuarios/logout.php">Logout</a>
                     </li>
                   </ul>
                 </div>
